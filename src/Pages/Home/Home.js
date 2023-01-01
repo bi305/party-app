cd35a78918b7fff8870f4dd554b5b1e19d16f414
bi305 <razab0715@gmail.com>
2023-01-02 00:08:14 +0500

    uploader added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Button, Checkbox, Form, Container } from "semantic-ui-react";
+import styles from "./home.module.css";
 
 const Home = () => {
 	return (
-		<Container  >
+		<Container className={styles.form_container}>
 			<Form>
 				<Form.Field>
 					<label>Name </label>
-					<input placeholder="First Name" />
+					<input type="text" placeholder="First Name" />
 				</Form.Field>
 				<Form.Field>
 					<label>surname</label>
-					<input placeholder="Last Name" />
+					<input type="text" placeholder="Last Name" />
 				</Form.Field>
 				<Form.Field>
 					<label>Name of Party</label>
-					<input placeholder="Last Name" />
+					<input type="text" placeholder="Last Name" />
+				</Form.Field>
+				<Form.Field>
+					<label>Upload your profile photo</label>
+					<input type="file" placeholder="Last Name" />
 				</Form.Field>
 				<Form.Field>
 					<Checkbox label="I agree to the Terms and Conditions" />
